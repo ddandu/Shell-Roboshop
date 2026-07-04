0a100f2eb941275878d69cf10684d5bc24362111
ddandu <devendra2402@gmail.com>
2026-07-04 22:19:32 +0530

    added commit

--- Shell-Robshop/Robosho.sh
+++ Shell-Robshop/Robosho.sh
@@ -2,10 +2,40 @@
 
 AMI_ID="mi-0220d79f3f480ecf5"
 SD_ID="sg-02e4e006dfaceac5c"
+ZONE_ID="Z08998102AQ5DAQ38NK0N" # replace with your ID
+DOMAIN_NAME="devresh.online"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-do 
- INSTACE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-02e4e006dfaceac5c --tag-specifications "ResourceType=instance,Tags=[{Key=Environment,Value=$Instance}]" --query 'Instances[0].InstanceId' --output text)
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.devresh.online
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # devresh.online
+    fi
 
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
